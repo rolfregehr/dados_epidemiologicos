7c11a930150f1bd13698e2825a49bd5f413d968f
rolfregehr <rolf.regehr@gmail.com>
2020-09-02 10:29:10 -0300

    fiz nada

--- Carga de dados PS.R
+++ Carga de dados PS.R
@@ -1,1 +1,33 @@
 library(tidyverse)
+
+library(read.dbc)
+
+
+arq = list.files("D:/Dados epidemiológicos/PS/")
+
+setwd("D:/Dados epidemiológicos/PS/")
+
+cont = 0
+
+for (i in arq) {
+  
+  if (cont == 0) {
+    
+    atend_ps = subset(read.dbc(i), select = c('UFMUN', 'DT_ATEND', 'CNS_PAC', 'IDADEPAC', 'SEXOPAC', 'RACACOR', 'CIDPRI', 'CIDASSOC', 'PERMANEN'))
+    
+  } 
+  else {
+    atend_ps = rbind(atend_ps, subset(read.dbc(i), select = c('UFMUN', 'DT_ATEND', 'CNS_PAC', 'IDADEPAC', 'SEXOPAC', 'RACACOR', 'CIDPRI', 'CIDASSOC', 'PERMANEN')))
+    atend_ps = atend_ps[!is.na(atend_ps$PERMANEN),]
+  }
+  
+  
+  print(i)
+  cont = cont+1
+  print(cont)
+  print(length(atend_ps$CNS_PAC))
+}
+
+teste <- read.dbc("PSRO1903.DBC")
+
+left_join()
